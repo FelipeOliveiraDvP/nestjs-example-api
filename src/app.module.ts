52a623832d22e1a326c60094cb08603a807d7d06
Felipe de Oliveira Souza <felipe.deoliveira@sp.senai.br>
2023-05-22 16:57:42 -0300

    feat: implementing public decorator

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,8 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import authConfig from './config/auth.config';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './auth/auth.guard';
 
 // TODO: Create separate config files: https://www.tomray.dev/nestjs-config
 @Module({
@@ -17,6 +19,12 @@ import authConfig from './config/auth.config';
     UsersModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+    AppService,
+  ],
 })
 export class AppModule {}
